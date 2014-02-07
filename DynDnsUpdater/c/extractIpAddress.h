ade6002b535b9e943d9c215d714b06870eb5ad2e
bradley selbrede <bks62464@yahoo.com>
2014-02-06 21:59:23 -0800

    correct the comment block explaining intent.

--- DynDnsUpdater/c/extractIpAddress.h
+++ DynDnsUpdater/c/extractIpAddress.h
@@ -1,10 +1,9 @@
 #if !defined DYNDNSUPDATER_EXTRACTIPADDRESS_H
 #define DYNDNSUPDATER_EXTRACTIPADDRESS_H
 
-// find a string that looks like an ip address in the given text string.
-// if found, return offsets to the start of the ip address string and
-// to one past the end of the ip address string. These are offsets 
-// from the begining of the user supplied text string
+// find a string that looks like an ipv4 address in the given text string.
+// if found, copy the ip address from the text to the buffer, buf.
+// returns zero if successful, non-zero otherwise.
 
 int extractIpAddress(const char* text, char* buf, int bufsize);
 
