08e746386d97679c43a77dd94eb8c918955e9703
bradselb <brad.selbrede@yahoo.com>
2014-10-08 23:03:46 -0700

    add Http Request
    this is just  my initial, sleepy thoughts. Obviously, this will all
    need to be fleshed out and possibly, re-thought out.

---
+++ lib/HttpRequest.c
@@ -0,0 +1,2 @@
+#include "HttpRequest.h"
+#include "inet.h"
---
+++ lib/HttpRequest.h
@@ -0,0 +1,22 @@
+#if !defined HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+struct HttpRequest;
+
+struct HttpRequest* http_alloc_request(void);
+
+void http_free_request(struct HttpRequest*);
+
+int http_set_host(struct HttpRequest*, const char* host);
+int http_set_resource(struct HttpRequest*, const char* resource);
+int http_set_user_agent(struct HttpRequest*, const char* user_agent);
+int http_set_basic_auth(struct HttpRequest*, const char* user, const char* pass);
+
+int http_to_string(struct HttpRequest*, char* buf, int bufsize);
+
+int http_send(struct HttpRequest* request, int timeout);
+
+int http_wait_reply(struct HttpRequest*, char* buf, int bufsize, int timeout);
+
+
+#endif // !defined HTTP_REQUEST_H
