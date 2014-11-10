624ce4522eaff82b248d63d4dbada61e903d1129
bradselb <Brad.Selbrede@yahoo.com>
2014-11-09 18:53:35 -0800

    major re-write to use the http package

--- DynDnsUpdater/tcl/updateDynDNS.tcl
+++ DynDnsUpdater/tcl/updateDynDNS.tcl
@@ -1,178 +1,143 @@
 #! /usr/bin/tclsh
 
+package require http
 package require base64
 
-set UpdateIp(host) {members.dyndns.org}
-set UpdateIp(port) {8245}
-set UpdateIp(result) {none}
-set UpdateIp(content) [list]
-set UpdateIp(timeout) {10000} ; # milliseconds
-set UpdateIp(done) 0
+set Version {v0.9 (09-Nov-2014)}
 
-#set UpdateIp(statefile) [file join / var tmp UpdateDynDNS_state.dat]
+
+# this stuff should come from a config file
+set DetectIp(url) {http://checkip.dyndns.org:8245}
+set DetectIp(timeout) 10000
+set UpdateIp(url) {http://members.dyndns.org:8245/nic/update/}
+set UpdateIp(timeout) 10000
+#set UpdateIp(statefile) [file join / var run UpdateDynDNS_state.dat]
 set UpdateIp(statefile) [file join [pwd] UpdateDynDNS_state.dat]
-set UpdateIp(enable) {false} ; # don't do anything unless good-to-go.
-set UpdateIp(prev_timestamp) [clock seconds] ; # default to now.
-set UpdateIp(prev_result) {none}
-set UpdateIp(prev_ip) {127.0.0.1}
 
-set DetectIp(host) {checkip.dyndns.org}
-set DetectIp(resource) {/}
-set DetectIp(port) {80}
-set DetectIp(myIp) {127.0.0.1}
-set DetectIp(timeout) {10000}  ; # milliseconds
-set DetectIp(done) 0
+# this stuff comes from the state file
+set PrevState(timestamp) [clock seconds] ; # default to now.
+set PrevState(myIp) 127.0.0.1
+set UpdateIp(prev_status) none
 
-#clock format [clock seconds] -format {%a, %d %b %Y %T %Z} -gmt true 
-
-proc sendHttpGetRequest {chan host resource {auth {NONE}}} { 
-   set date [clock format [clock seconds] -format {%a, %d %b %Y %T %Z} -gmt 1]
-   puts $chan "GET $resource HTTP/1.0"
-   puts $chan "Host: $host"
-   if { {NONE} != $auth } {
-      puts $chan "Authorization: Basic $auth"
-   }
-   puts $chan "User-agent: Self - Brad's DynDNS Update Client (Tcl version) - v0.6 (08-Oct-2014)"
-   puts $chan "Date: $date"
-   puts $chan "Connection: close"
-   puts $chan ""
-}
-
-proc loadState {} {
-   global UpdateIp
-   if { [catch {open $UpdateIp(statefile) "r"} f] } {
-      puts stdout "failed to open state file for read"
-   } else {
-      if { ![eof $f] && ![catch {gets $f line}] } { set UpdateIp(enable) "$line" }
-      if { ![eof $f] && ![catch {gets $f line}] } { set UpdateIp(prev_timestamp) "$line" } 
-      if { ![eof $f] && ![catch {gets $f line}] } { set UpdateIp(prev_result) "$line" } 
-      if { ![eof $f] && ![catch {gets $f line}] } { set UpdateIp(prev_ip) "$line" } 
+proc loadState {path} {
+    global UpdateIp
+    if { [catch {open $path "r"} f] } {
+        puts stdout "failed to open state file for read"
+    } else {
+        if { ![eof $f] && ![catch {gets $f line}] } {
+            set PrevState(timestamp) "$line"
+        }
+        if { ![eof $f] && ![catch {gets $f line}] } {
+            set result [split $line]
+            set UpdateIp(prev_status) [lindex $result 0]
+            set PrevState(myIp) [lindex $result 1]
+        }
       close $f
    }
 }
 
-proc saveState {} {
+proc saveState {path result} {
    global UpdateIp
    global DetectIp
-   if { [catch {open $UpdateIp(statefile) "w"} f] } {
+   if { [catch {open $path "w"} f] } {
       puts stdout "failed to open state file for write"
    } else {
       set timestamp [clock format [clock seconds] -format {%d-%b-%Y %H:%M:%S}]
-      puts $f "enabled"
       puts $f "$timestamp"
-      puts $f "$UpdateIp(result)"
-      puts $f "$DetectIp(myIp)"
+      puts $f "$result"
       close $f
    }
 }
 
 
-proc receiveDetectIpReply {chan} {
-   global DetectIp
-   if { [eof $chan] } {
-      set DetectIp(done) {EOF}
-   } elseif { [catch {gets $chan reply}] } {
-      set DetectIp(done) {EXCEPTION}
-   } else {
-      # puts stderr "$reply" 
-      set matches [regexp -nocase -expanded  {([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)}  $reply where addr]
-      if { 0 < $matches } { 
-         set DetectIp(myIp)  "$addr" 
-         set DetectIp(done) {OK}
-      }
-   }
-}
 
-proc detectIp {} {
-   global DetectIp
-   set sock [socket -async $DetectIp(host) $DetectIp(port)]
-   fconfigure $sock -blocking 1 -buffering line
-   fileevent $sock readable [list receiveDetectIpReply $sock]
-   fileevent $sock writable [list sendHttpGetRequest $sock $DetectIp(host) $DetectIp(resource)]
 
-   after $DetectIp(timeout) [list set DetectIp(done) {TIMEOUT}]
-   vwait DetectIp(done)
-   close $sock
+proc detectIp {url timeout} {
+    set myIp {} ; # what this fctn returns
+    set token [::http::geturl $url -timeout $timeout]
+    set status [string tolower [::http::status $token]]
+    set body [::http::data $token]
+    http::cleanup $token
 
-   puts stderr "detect ip: $DetectIp(done)"
+    if {[string match {ok} $status]} {
+        set expression {([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)}
+        set count [regexp -nocase -expanded $expression $body all addr]
+        if {$count > 0} {
+            set myIp $addr
+        } else {
+            set myIp "could not find an IP address in the http response"
+        }
+    }
 
-   return "$DetectIp(myIp)"
+   return -code $status $myIp
 }
 
 
-proc receiveUpdateIpReply {chan} {
-   global UpdateIp
-   if { [eof $chan] } {
-      set UpdateIp(done) {EOF}
-   } elseif { [catch {gets $chan reply}] } {
-      set UpdateIp(done) {EXCEPTION}
-   } else {
-      lappend UpdateIp(content) $reply
-      # puts stderr "$reply"
-   }
-}
 
-proc updateIp {myhostname myipaddr auth} {
-   global UpdateIp
-   set resource "/nic/update?hostname=$myhostname&myip=$myipaddr"
-   set sock [socket -async $UpdateIp(host) $UpdateIp(port)]
-   fconfigure $sock -blocking 1 -buffering line
-   fileevent $sock readable [list receiveUpdateIpReply $sock]
-   fileevent $sock writable [list sendHttpGetRequest $sock $UpdateIp(host) $resource $auth]
+proc updateIp {url timeout user passwd hostname myip} {
+    global Version
 
-   after $UpdateIp(timeout) [list set UpdateIp(done) {TIMEOUT}]
-   vwait UpdateIp(done)
-   close $sock
+    set useragent "Self - Brad's DynDNS Update Client (Tcl version) - $Version"
+    ::http::config -useragent $useragent
 
-   puts stderr "update ip: $UpdateIp(done)"
+    set hdrs [list Authorization "Basic [base64::encode $user:$passwd]"] 
+    set query [::http::formatQuery hostname $hostname myip $myip]
+    set token [::http::geturl "$url?$query" -headers $hdrs -timeout $timeout]
+    set status [string tolower [::http::status $token]]
+    set body [::http::data $token]
+    # for debugging
+    #upvar #0 $token state
+    #puts stderr "requested url: '$state(url)'"
+    http::cleanup $token
 
-   return [getUpdateIpResult]
-}
+    #puts stderr "$status"
+    #puts stderr "$body"
 
-proc getUpdateIpResult {} {
-   global UpdateIp
-   set UpdateIp(result) {fail}
-   if { 0 < [llength $UpdateIp(content)] } {
-      set content [lindex $UpdateIp(content) end]
-      set match [regexp -nocase -expanded {^([\w]+) [\s]+ ([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)} $content where result ip]
-      if { 0 < $match } {
-         set UpdateIp(new_ip) $ip
-         set UpdateIp(result) $result
-      }
-   }
-   return "$UpdateIp(result)"
+   return -code $status $body
 }
 
 
-proc updateDynDNS {username password myHostName} {
-   global UpdateIp
-   set myAuth [base64::encode "$username:$password"]
 
-   loadState
+proc updateDynDNS {username password hostname} {
+    global UpdateIp
+    global DetectIp
 
-   if { {enabled} == $UpdateIp(enable) && {good} == $UpdateIp(prev_result) } {
-      set myIp [detectIp]
-      set prevIp "$UpdateIp(prev_ip)"
+    puts stdout "----------------------------------------------------------------------------"
+    set timestamp [clock format [clock seconds] -format {%d-%b-%Y %H:%M:%S}]
+    puts stdout "$timestamp"
 
-      puts stdout "current  ip addr: $myIp"
-      puts stdout "previous ip addr: $prevIp"
+    loadState $UpdateIp(statefile)
 
-      if { $myIp != $prevIp && $myIp != {127.0.0.1} } {
-         updateIp $myHostName $myIp $myAuth
-         saveState
-         puts stdout "$UpdateIp(result)"
-      }
-   }
+    set prevIp "$PrevState(myIp)"
+    set myIp {127.0.0.1}
+
+    if {[string match -nocase {good} $UpdateIp(prev_status)]} {
+        if {[catch {detectIp $DetectIp(url) $DetectIp(timeout)} result]} {
+            puts stderr "FAIL: detectIp $result"
+        } else {
+            set myIp "$result"
+        }
+    }
+
+
+    puts stdout "previous ip addr: $prevIp"
+    puts stdout "current  ip addr: $myIp"
+
+    if { $myIp != $prevIp && $myIp != {127.0.0.1} } {
+        if {[catch {updateIp $UpdateIp(url) $UpdateIp(timeout) $username $password $hostname $myIp} result]} {
+            puts stderr "FAIL: updateIp $result"
+        } else {
+            puts stdout "$result"
+            saveState $UpdateIp(statefile) $result
+        }
+    }
 }
 
 
-puts stdout "----------------------------------------------------------------------------"
-set timestamp [clock format [clock seconds] -format {%d-%b-%Y %H:%M:%S}]
-puts stdout "$timestamp"
 
 set username {test}
 set password {test}
 set hostname {test.dyndns.org}
-updateDynDNS $username $password $hostname
 
+updateDynDNS $username $password $hostname
 
