932725a06442f8e8a4998280541d8da1f747fd18
bradselb <Brad.Selbrede@yahoo.com>
2014-12-27 17:38:37 -0800

    add a simple hexdump function to the library.

---
+++ lib/hexdump.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// --------------------------------------------------------------------------
+int hexdump(const char* buf, int size)
+{
+	int k = 0;
+	for (int i=0; i<size; ++i) {
+		if (0 == i%16) {
+			if (0 < i) {
+				printf("| ");
+				for (int j=k; j<i; ++j) {
+					if(isprint(buf[j]))
+						printf("%c", buf[j]);
+					else
+						printf(".");
+				}
+				k = i;
+			}
+			printf("\n%04x | ", i);
+		}
+		printf("%02x ", buf[i]);
+	} // for
+
+	// need to insert spaces when size%16 != 0
+	for (int i=size%16; i%16; ++i) {
+		printf("   ");
+	}
+
+	// now, finish up the line by prining the ascii for the tail
+	printf("| ");
+	for (int j=k; j<size; ++j) {
+		if(isprint(buf[j]))
+			printf("%c", buf[j]);
+		else
+			printf(".");
+	}
+
+	return 0;
+}
+
+
---
+++ lib/hexdump.h
@@ -0,0 +1,6 @@
+#ifndef HEXDUMP_H
+#define HEXDUMP_H
+
+int hexdump(const char*, int);
+
+#endif
