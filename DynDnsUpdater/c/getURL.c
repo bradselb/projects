235cad1a78a8244560871396031dc5270ce8c204
bradselb <brad.selbrede@yahoo.com>
2015-01-18 13:55:15 -0800

    remove all dependence upon libcurl.

--- DynDnsUpdater/c/getURL.c
+++ DynDnsUpdater/c/getURL.c
@@ -1,167 +1,11 @@
 #include "getURL.h" 
+#include "HttpRequest.h"
+#include "inet.h"
 
 #include <string.h> // memset(), memcpy()
 #include <stdio.h> // fprintf()
-
-
-#if defined USE_CURL_LIBRARY
-
-#define CURL_NO_OLDIES 1
-#include <curl/curl.h>
-#include <curl/easy.h>
-
-
-
-// --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
-struct UserData
-{
-   char* begin;
-   char* end;
-   size_t size;
-};
-
-// --------------------------------------------------------------------------
-int initUserData(struct UserData* ud, char* buffer, int size)
-{
-   int rc = 0;
-   if ( !ud || !buffer || size < 2 ) {
-      rc = -1; // fail.
-   } else {
-      memset(buffer, 0, size);
-      ud->begin = buffer;
-      ud->end = buffer;
-      ud->size = size;
-      rc = 0;
-   }
-   return rc;
-}
-
-// --------------------------------------------------------------------------
-size_t appendUserData(struct UserData* ud, char* p, size_t n)
-{
-   size_t count = 0;
-
-   if ( p && ud && ud->begin && ud->end ) {
-      while ( count < n && (ud->end - ud->begin) + 1 < ud->size ) {
-         *(ud->end)++  = *p++;
-         ++count;
-      }
-   }
-   return count;
-}
-
-
-// --------------------------------------------------------------------------
-size_t response(char* ptr, size_t size, size_t n, void* userdata)
-{
-   size_t bytesHandled;
-   
-   bytesHandled = appendUserData((struct UserData*)(userdata), ptr, n*size);
-   
-   return bytesHandled;
-}
-
-
-
-// --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
-// --------------------------------------------------------------------------
-int getURL(char* buf, int bufsize, const char* url, const char* agent, const char* user, const char* pass)
-{
-   int rc = 0;
-   
-   struct UserData userData;
-
-   CURLcode ec;
-   CURL* curl = 0;
-   //curl = curl_easy_init();
-
-   if ( initUserData(&userData, buf, bufsize) ) {
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, "");
-   }
-   else if ( 0 == (curl=curl_easy_init()) ) {
-      rc = -1;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, "curl initialization failed");
-   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L)) ) {
-      rc =ec;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__,curl_easy_strerror(ec));
-   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_URL, url)) ) {
-      rc = 1;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_USERAGENT, agent)) ) {
-      rc = 2;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0)) ) {
-      rc = 3;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( user && CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_USERNAME, user)) ) {
-      rc = 5;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( pass && CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_PASSWORD, pass)) ) {
-      rc = 6;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( user && pass && CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC)) ) {
-      rc = 7;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-// can ignore headers.
-//   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header)) ) {
-//      rc = 8;
-//      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-//   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, response)) ) {
-      rc = 9;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( CURLE_OK != (ec = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &userData)) ) {
-      rc = 10;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else if ( CURLE_OK != (ec = curl_easy_perform(curl)) ) {
-      rc = 11;
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, curl_easy_strerror(ec));
-   }
-   else {
-      rc = 0; // success!
-      fprintf(stderr,"(%s:%d) %s\n", __FILE__, __LINE__, "OK");
-   }
-
-   curl_easy_cleanup(curl);
-
-   return rc;
-}
-
-#else
 #include <stdlib.h>  // malloc()
 #include <unistd.h> // close()
-
-#include "HttpRequest.h"
-#include "hexdump.h"
-#include "inet.h"
-
-// this is a stub that can be substituted for the above....for developemnt or test for example. 
-int getURL(char* buf, int bufsize, const char* host, const char* agent, const char* user, const char* pass)
-{
-   int rc = 0;
-   fprintf(stderr, "(%s:%d) %s(), host: %s, agent: %s\n", __FILE__, __LINE__, __FUNCTION__, host, agent);
-   if (buf) {
-      if (0 == strncmp(host, "checkip", 6)) {
-         strncpy(buf, "<html><head><title>Current IP Check</title></head><body>Current IP Address: 198.87.22.17</body></html>", bufsize);
-      } else if (0 == strncmp(host, "members", 7)) {
-         strncpy(buf, "good 142.21.17.42", bufsize);
-      }
-   }
-   return rc; 
-}
 
 
 int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, const char* agent, const char* auth, char* reply, int bufsize)
@@ -194,16 +38,14 @@ int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, c
         fprintf(stderr, "http_alloc_request() failed.\n");
         goto exit;
     }
-
-    //rc = http_set_port(request, 80);
     if (agent) {
         rc = http_set_user_agent(request, agent);
     }
-
     if (auth) {
         rc = http_set_basic_auth(request, auth);
     }
 
+    // ask the HTTP request object to output a plain text string representation.
     rc = http_to_string(request, request_str, request_bufsize);
     if (rc != 0) {
         fprintf(stderr, "%s(), http_to_string() returned: %d\n", __FUNCTION__, rc);
@@ -216,10 +58,6 @@ int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, c
         printf("%s", request_str);
         fflush(stdout);
     }
-    if (verbose > 1) {
-        hexdump(request_str, 256);
-    }
-
 
     // connect to the web server. 
     sock = inet_connect_tcp(hostname, "http");
@@ -235,9 +73,12 @@ int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, c
     if (result < 0) {
         fprintf(stderr, "%s(), inet_send() failed.\n", __FUNCTION__);
         goto exit;
-    } else if (verbose) {
-        fprintf(stderr, "expected to send %lu bytes.\n", bytecount);
-        fprintf(stderr, "actually sent %ld bytes.\n", result);
+    }
+    
+    if (verbose) {
+        printf("expected to send %lu bytes.\n", bytecount);
+        printf("actually sent %ld bytes.\n", result);
+        fflush(stdout);
     }
 
     // and wait for the web server to reply
@@ -245,13 +86,14 @@ int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, c
     if (result < 0) {
         fprintf(stderr, "%s(), inet_wait_reply() failed.\n", __FUNCTION__);
         goto exit;
-    } else if (verbose){
-        fprintf(stderr, "received %ld bytes.\n\n", result);
+    }
+    
+    if (verbose){
+        printf("received %ld bytes.\n\n", result);
+        printf("%s\n", reply);
+        fflush(stdout);
     }
 
-    if (verbose >0) {
-        printf("%s\n", reply);
-    }
 
 exit:
     if (sock > 0) {
@@ -264,10 +106,7 @@ exit:
 
     http_free_request(request);
 
- return rc;
+    return rc;
 }
 
-#endif
-
-// --------------------------------------------------------------------------
 
--- DynDnsUpdater/c/getURL.h
+++ DynDnsUpdater/c/getURL.h
@@ -1,12 +1,18 @@
 #if !defined GETURL_H
 #define GETURL_H
 
-// get URL 
-// copies content of page at URL to user supplied buffer. 
-// caller owns the buffer. 
-
-int getURL(char* buf, int bufsize, const char* url, const char* userAgent, const char* user, const char* pass );
-
+// this function does a complete HTTP transaction.
+// for example, if you wanted to get the URL...
+// http://http://www.dot.ca.gov/cgi-bin/roadscell.cgi?roadnumber=108
+// then, 
+//   1. allocate a buffer large enough to hold the WHOLE http response 
+//   2. and call this function with
+//      hostname = "www.dot.ca.gov",
+//      resource = "/roadscell.cgi?roadnumber=108"
+//      agent = 0, auth = 0, and
+//      reply = buffer allocated in #1 and bufsize = associate buffer size.
+//
+// returns non-zero on failure, otherwise returns zero.
 int sendHttpRequestAndWaitForReply(const char* hostname, const char* resource, const char* agent, const char* auth, char* reply, int bufsize);
 
 #endif //!defined GETURL_H
